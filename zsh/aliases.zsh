5721bddb4ce4e2ba492ecb0af235170c729555b5
Moshe Beladev <moshebe@users.noreply.github.com>
2022-09-16 17:26:51 +0300

    put back common aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,12 @@
 alias reload!=". ~/.zshrc"
 alias ..="cd .."
 alias ...="cd ../.."
+
+alias ll='ls -alF'
+alias dl="cd ~/Downloads"
+alias dt="cd ~/Desktop"
+alias path='echo -e ${PATH//:/\\n}'
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en0"
+alias urlencode='python3 -c "import sys, urllib.parse as up; print(up.quote_plus(sys.argv[1]))"'
+alias urldecode='python3 -c "import sys, urllib.parse as up; print(up.unquote_plus(sys.argv[1]))"'
