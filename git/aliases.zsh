58d0eef97a7df1fbe6a9506efdd7a0b7aae0da50
Moshe Beladev <moshebe@users.noreply.github.com>
2022-09-16 09:47:23 +0300

    git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,3 +1,5 @@
+export GIT_AUTHOR="Moshe Beladev"
+
 alias gl="git pull --rebase"
 alias gwip="git wip && git push"
 
@@ -18,11 +20,40 @@ function changelog() {
 			if [ -d $repo/.git ]
 			then
 				cd $repo
-				git log --author="Or Elimelech" --pretty=format:"%h - %an, %ar : %s" --after="$d"
+				git log --author="${GIT_AUTHOR}" --pretty=format:"%h - %an, %ar : %s" --after="$d"
 				cd ../
 			fi
 		done
 	else
-		git log --author="Or Elimelech" --pretty=format:"%h - %an, %ar : %s" --after="$d"
+		git log --author="${GIT_AUTHOR}" --pretty=format:"%h - %an, %ar : %s" --after="$d"
 	fi
 }
+
+
+# Outputs the name of the current branch
+# Usage example: git pull origin $(git_current_branch)
+# Using '--quiet' with 'symbolic-ref' will not cause a fatal error (128) if
+# it's not a symbolic ref, but in a Git repo.
+function git_current_branch() {
+  local ref
+  ref=$(command git symbolic-ref --quiet HEAD 2> /dev/null)
+  local ret=$?
+  if [[ $ret != 0 ]]; then
+    [[ $ret == 128 ]] && return  # no git repo.
+    ref=$(command git rev-parse --short HEAD 2> /dev/null) || return
+  fi
+  echo ${ref#refs/heads/}
+}
+
+alias gti='git'
+alias ga='git add'
+alias gaa='git add --all'
+alias gco='git checkout'
+alias gd='git diff'
+alias ggpull='git pull origin $(git_current_branch)'
+alias ggpush='git push origin $(git_current_branch)'
+alias gl="git pull --rebase"
+alias glog="git log --graph --pretty=format:'%C(yellow)%h %Cred%ad %Cblue%an%Cgreen%d %Creset%s' --date=short"
+alias gfetch='git fetch --all'
+alias glasttag='git describe --tags `git rev-list --tags --max-count=1`'
+alias gwch='git whatchanged -p --abbrev-commit --pretty=medium'
