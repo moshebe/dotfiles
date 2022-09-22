172ab655f9b8a84e38a8e627eba0d13f0d498331
Moshe Beladev <moshebe@users.noreply.github.com>
2022-09-22 09:41:52 +0300

    fix paths

--- go/install.sh
+++ go/install.sh
@@ -2,6 +2,8 @@
 readonly PACKAGES=(
 	golang.org/x/tools/gopls@latest
 	github.com/bufbuild/buf-language-server/cmd/bufls@latest
+	golang.org/x/tools/cmd/goimports@latest
+	github.com/golang/mock/mockgen@latest
 )
 
 if which go > /dev/null 2>&1
