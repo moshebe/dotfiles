f498528e03da4f5da7eaccf5a2f5559cac706ef7
Moshe Beladev <moshebe@users.noreply.github.com>
2022-10-29 12:16:05 +0300

    new-uuid, finnotify trigger and updat torqfiles

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -11,4 +11,6 @@ alias localip="ipconfig getifaddr en0"
 alias urlencode='python3 -c "import sys, urllib.parse as up; print(up.quote_plus(sys.argv[1]))"'
 alias urldecode='python3 -c "import sys, urllib.parse as up; print(up.unquote_plus(sys.argv[1]))"'
 
-alias new-uuid="uuidgen | tr '[:upper:]' '[:lower:]'"
+alias new-uuid="uuidgen | tr '[:upper:]' '[:lower:]' | tr -d '\n'"
+
+alias finnotify="curl https://agile-wildwood-94022.herokuapp.com/run"
