8132db1e3387c6aa48293720de1cb9b45df363d1
Moshe Beladev <moshebe123@gmail.com>
2024-05-01 21:34:12 +0300

    updates

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,9 +9,6 @@ alias path='echo -e ${PATH//:/\\n}'
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en0"
 
-alias urlencode='python3 -c "import sys, urllib.parse as up; print(up.quote_plus(sys.argv[1]))"'
-alias urldecode='python3 -c "import sys, urllib.parse as up; print(up.unquote_plus(sys.argv[1]))"'
-
 alias chromekill="ps ux | grep '[C]hrome Helper --type=renderer' | grep -v extension-process | tr -s ' ' | cut -d ' ' -f2 | xargs kill"
 
 alias dbdevproxy="cloud_sql_proxy -instances=stackpulse-development:europe-west1:stackpulsedb=tcp:5432"
